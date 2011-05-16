0c8b5a0fb9e167e50d14f021d7519968f8907f02
kenneth koontz <kenneth koontz>
2011-05-15 20:21:34 -0700

    added todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=60)
+    created = models.DateTimeField(auto_now_add=True)
+    priority = models.IntegerField(default=0)
+    difficulty = models.IntegerField(default=0)
+    done = models.BooleanField(default=False)
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'priority', 'difficulty', 'created', 'done']
+    search_fields = ['name']
+
+admin.site.register(Item, ItemAdmin)
